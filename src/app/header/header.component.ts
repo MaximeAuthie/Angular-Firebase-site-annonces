bc90618aa62d551812eb6fadc6ba23ec95cde5c6
Maxime Authié <authie.maxime@orange.fr>
2023-12-15 18:28:14 +0100

    Ajout déconnexion

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from '../interfaces/user';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+
+  currentUserSubscription!: Subscription;
+  currentUser!: User;
 
   title = 'CarSell 9000';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.currentUserSubscription = this.authService.currentUserSubject.subscribe({
+      next: user => this.currentUser = <User>user,
+      error: console.error
+    });
+  }
+
   getTitle():string {
     return this.title;
+  }
+
+  onSignOut(): void {
+    this.authService.signoutUser()
+      .then(() => this.router.navigate(['/home']))
+      .catch(console.error);
+  }
+
+  ngOnDestroy(): void {
+    this.currentUserSubscription.unsubscribe();
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private auth: AngularFireAuth) { }
+  currentUserSubject = new BehaviorSubject<User | null>(null);
+
+  constructor(private auth: AngularFireAuth) {
+
+    //? Détecter un changement dans l'état d'authentification pour émettre le nouvel état via un suscribe
+    this.auth.onAuthStateChanged(user => {
+      this.currentUserSubject.next(user);
+    }, console.error);
+  }
 
   signupUser(email:string, password: string): Promise<any> {
     return new Promise ((resolve, reject) => {
@@ -22,6 +32,17 @@ export class AuthService {
     return new Promise ((resolve, reject) => {
       this.auth.signInWithEmailAndPassword(email, password)
         .then(resolve)
+        .catch(reject)
+    })
+  }
+
+  signoutUser(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.auth.signOut()
+        .then(() => {
+          this.currentUserSubject.next(null);
+          resolve();
+        })
         .catch(reject)
     })
   }
