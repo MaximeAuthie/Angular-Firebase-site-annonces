64133e6e749f64107ee3ba3213592557b1e34c19
Maxime Authié <authie.maxime@orange.fr>
2023-12-19 18:53:26 +0100

    Ajout modification MDP

--- src/app/account/edit-auth-infos/edit-auth-infos.component.ts
+++ src/app/account/edit-auth-infos/edit-auth-infos.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-edit-auth-infos',
   templateUrl: './edit-auth-infos.component.html',
   styleUrls: ['./edit-auth-infos.component.scss']
 })
-export class EditAuthInfosComponent {
+export class EditAuthInfosComponent implements OnInit{
 
+  @Input() currentUser!: User;
+
+  passwordForm!: FormGroup;
+
+  constructor(private modalService: NgbModal, private formBuilder: FormBuilder, private authService: AuthService) {}
+
+  ngOnInit(): void {
+      this.initPasswordForm();
+  }
+
+  initPasswordForm(): void {
+    this.passwordForm = this.formBuilder.group({
+      oldPassword: ['', [Validators.required]],
+      newPassword: ['', [Validators.required, Validators.minLength(8)]],
+      newPasswordConfirm: ['', [Validators.required]]
+    })
+  }
+
+  onEditPasswordForm(modal: any): void {
+    this.modalService.open(modal, {centered: true})
+  }
+
+  onSubmitPasswordForm(): void {
+    this.authService.signinUser(<string>this.currentUser.email, this.passwordForm.value.oldPassword)
+      .then(() => {
+        this.currentUser.updatePassword(this.passwordForm.value.newPassword)
+          .then(() => {
+            this.modalService.dismissAll();
+            this.passwordForm.reset();
+          })
+          .catch(console.error)
+      })
+      .catch(console.error)
+  }
 }
