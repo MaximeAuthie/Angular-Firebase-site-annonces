7107ef5cfeeb146054906173979f5cf25cb1f80d
Maxime Authié <authie.maxime@orange.fr>
2023-12-21 11:30:18 +0100

    Ajout card offres page d'accueil

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { OffersService } from '../services/offers.service';
+import { Offer } from '../interfaces/offer';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy{
+
+  offersSubscription!: Subscription;
+
+  offers: Offer[] = [];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private offerService: OffersService
   ) {}
 
-  onLogin(): void {
-    this.router.navigateByUrl('/admin/dashboard');
+  ngOnInit(): void {
+      this.initOffers();
+  }
+
+  initOffers(): void {
+    this.offersSubscription = this.offerService.offersSubject.subscribe({
+      next: offers => this.offers = offers,
+      error: console.error
+    })
+
+    //? Après avoir souscrit à this.offerService.offersSubject, on déclenche la méthode this.offerService.getOffers() pour récupérer la liste des offres depuis Firebase
+    this.offerService.getOffers();
+  }
+
+  ngOnDestroy(): void {
+      this.offersSubscription.unsubscribe();
   }
 
 }
